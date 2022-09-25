3ebf4948392040f8a8817ebf3aecf2f0bc18108f
xiraynedev <xiraynedev@gmail.com>
2022-09-24 20:23:07 -0400

    move meta tag to _app.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta name='viewport' content='width=device-width,initial-scale=1.0' />
         <meta name='author' content='xiraynedev' />
         <meta
           name='description'
